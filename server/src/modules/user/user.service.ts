fe01b4cdae176934bcd1d8d9e0593273ba96c041
Hussein Hassan <hussein.h.tech@gmail.com>
2025-04-14 14:57:47 +0300

    Update user.service.ts

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { IUser, User } from './user.schema';
 import { Model } from 'mongoose';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<IUser>) {}
 
   async findAll() {
-    return await this.userModel.create<User>({
-      name: 'John Doe',
-    });
+    return await this.userModel.find();
   }
 }
