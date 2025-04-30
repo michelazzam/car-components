64a0ab199bd549f3e60c363fde3a86552522505f
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-04-30 12:41:25 +0300

    fix: table style

--- client/shared/ReactTablePaginated/TableStyle.tsx
+++ client/shared/ReactTablePaginated/TableStyle.tsx
@@ -1,6 +1,6 @@
 export default function TableStyle({ children }: { children: any }) {
   return (
-    <div className="mb-2 flex flex-col">
+    <div className="mb-2  flex flex-col">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="inline-block min-w-full sm:px-6 lg:px-8">
           <div className="overflow-hidden border border-gray-200 rounded">
