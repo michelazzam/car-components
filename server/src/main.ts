499e78269fc5a04f8963ebb421471a999039173f
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-13 13:54:00 +0300

    enable 3001 port

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
     credentials: true,
   });
