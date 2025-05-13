b4f4ef74bf31e284aedb78b0c80dee2a43de5d39
Hussein Hassan <hussein.h.tech@gmail.com>
2025-05-13 14:12:49 +0300

    single product suppliers and customers APIs

--- server/src/modules/item/item.module.ts
+++ server/src/modules/item/item.module.ts
@@ -3,10 +3,16 @@ import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Item, ItemSchema } from './item.schema';
+import { Purchase, PurchaseSchema } from '../purchase/purchase.schema';
+import { Invoice, InvoiceSchema } from '../invoice/invoice.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Item.name, schema: ItemSchema }]),
+    MongooseModule.forFeature([
+      { name: Item.name, schema: ItemSchema },
+      { name: Purchase.name, schema: PurchaseSchema },
+      { name: Invoice.name, schema: InvoiceSchema },
+    ]),
   ],
   controllers: [ItemController],
   providers: [ItemService],
