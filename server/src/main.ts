3d44ec15ed33a47e992b57bc03b2ed02acbf149d
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-14 09:14:34 +0300

    trying to allow all origins

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,19 @@ async function bootstrap() {
   const app = await NestFactory.create(MainModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:3003',
+      'http://localhost:3004',
+      'http://localhost:3005',
+      'http://localhost:3006',
+      'http://localhost:3007',
+      'http://localhost:3008',
+      //allow all origins
+      '*',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'PUT'],
     credentials: true,
   });
