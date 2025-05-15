cbe057c17ca8d6e8c7657b93d02bfe8b4e26e630
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-15 10:22:06 +0300

    feat: loading .env from local env

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,9 @@ use tauri_plugin_shell::init as shell_plugin;
   use std::io::Write;
 use tauri_plugin_shell::ShellExt;
 use tauri_plugin_shell::process::CommandEvent;
+use dotenv::dotenv;
 fn main() {
+    dotenv().ok();
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(shell_plugin())
@@ -14,11 +16,11 @@ fn main() {
             io::stderr().flush().unwrap();
             let sidecar_command = app.shell().sidecar("server").unwrap();
             let (mut rx, mut child) = sidecar_command
-                .env("NODE_ENV", "development")
-                .env("PORT", "8000")
-                .env("DATABASE_URL", "mongodb://localhost:27017")
-                .env("BACKUP_DATABASE_URL", "mongodb+srv://husseinhopehassan:dlM1aPjAoPpfFc12@cluster0.t9khsc5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-                .args(["--port", "8000"])
+                .env("NODE_ENV", dotenv!("NODE_ENV"))
+                .env("PORT", dotenv!("PORT"))
+                .env("DATABASE_URL", dotenv!("DATABASE_URL"))
+                .env("BACKUP_DATABASE_URL", dotenv!("BACKUP_DATABASE_URL"))
+                .args(["--port", dotenv!("PORT")])
                 .spawn()
                 .expect("failed to spawn sidecar");
 
