da2175017f094a516f8d2b43a71ace27d53a7be0
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-05-19 12:54:19 +0300

    feat: caisse history table

--- client/api-hooks/caisse/use-list-caisse-records.ts
+++ client/api-hooks/caisse/use-list-caisse-records.ts
@@ -1,11 +1,30 @@
 import { useReadData } from "@/api-service/useReadData";
+import { Pagination } from "@/components/admin/Pagination";
 import { API } from "@/constants/apiEndpoints";
 
-export const useGetCaisse = () => {
+export const useListCaisseRecords = (params: {
+  pageIndex: number;
+  pageSize: number;
+  startDate?: string;
+  endDate?: string;
+}) => {
   return useReadData<{
-    amount: number;
+    caisseHistory: CaisseHistoryRecord[];
+    pagination: Pagination;
   }>({
-    queryKey: ["caisse-history"],
+    queryKey: ["caisse-history", params],
     endpoint: API.listCaisse,
+    params,
   });
+};
+
+export type CaisseHistoryRecord = {
+  _id: string;
+  date: string;
+  closedAmount: number;
+  openedAmount: number;
+  closedAt: string | null;
+  openedAt: string | null;
+  createdAt: string;
+  updatedAt: string;
 };
