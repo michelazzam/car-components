83bcc92afd2acc45581b334fff2fddd188984edb
Hussein Hassan <hussein.h.tech@gmail.com>
2025-06-10 15:06:40 +0300

    Update loans-transactions.controller.ts

--- server/src/modules/loans-transactions/loans-transactions.controller.ts
+++ server/src/modules/loans-transactions/loans-transactions.controller.ts
@@ -1,10 +1,8 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { LoansTransactionsService } from './loans-transactions.service';
 import { ApiTags } from '@nestjs/swagger';
-import { IsPublic } from '../user/decorators/isPublic.decorator';
 import { GetLoansTransactionsDto } from './dto/get-loans-transactions.dto';
 
-@IsPublic()
 @ApiTags('Loans Transactions')
 @Controller({ version: '1', path: 'loans-transactions' })
 export class LoansTransactionsController {
