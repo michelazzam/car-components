f050861d08fc2b82c54d9e93b5d6843b65d33128
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-07-25 10:25:35 +0300

    feat: auth provider fixes

--- client/api-hooks/caisse/use-get-caisse.ts
+++ client/api-hooks/caisse/use-get-caisse.ts
@@ -1,12 +1,15 @@
 import { useReadData } from "@/api-service/useReadData";
 import { API } from "@/constants/apiEndpoints";
+import useAuth from "../useAuth";
 
 export const useGetCaisse = () => {
+  const { user } = useAuth();
   return useReadData<{
     isCaisseOpen: boolean;
     caisse: number;
   }>({
     queryKey: ["caisse"],
     endpoint: API.getCaisse,
+    enabled: !!user,
   });
 };
