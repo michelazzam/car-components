1c864fa9008f75afe250245e58bb29cefee6d339
mohammadNouredine <mohammadnouredine.cs@gmail.com>
2025-08-05 15:25:41 +0300

    2->3

--- client/pages/sign-in/index.tsx
+++ client/pages/sign-in/index.tsx
@@ -98,7 +98,7 @@ const Firebaselogin = () => {
                 Designed & Developed by{" "}
                 <span className="text-[#0076FA] mx-1">
                   <b> Advanced Meta Solutions </b>
-                  v2.0.0
+                  v3.0.0
                 </span>
               </a>
             </div>
